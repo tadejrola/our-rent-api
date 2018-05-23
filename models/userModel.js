64cf96b29afdd6964b766e9163298ff6d8ee275e
1tomy1 <tomi1995.jerenko@gmail.com>
2018-05-23 19:15:15 +0200

    FixedUser

--- models/userModel.js
+++ models/userModel.js
@@ -2,15 +2,13 @@ const Joi = require('joi');
 
 module.exports = Joi.object().keys({
     id: Joi.number().integer(),
-    firstName: Joi.string().required(),
-    lastName: Joi.string().required(),
-    username: Joi.string().required(),
-    password: Joi.string().required(),
-    phoneNumber: Joi.string().required(),
-    job: Joi.string().required(),
-    education: Joi.string().required(),
-    smoker: Joi.boolean().required(),
-    email: Joi.string().required(),
-    address: Joi.string().required(),
-    image: Joi.string().required()
+    firstName: Joi.string(),
+    lastName: Joi.string(),
+    username: Joi.string(),
+    phoneNumber: Joi.string(),
+    job: Joi.string(),
+    education: Joi.string(),
+    smoker: Joi.boolean(),
+    address: Joi.string(),
+    image: Joi.string()
 });
