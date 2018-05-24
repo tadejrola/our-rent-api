27f85ddd5b19b1f366b09959ea865102c2c141dc
unknown <tadej.rola@gmail.com>
2018-05-24 18:28:46 +0200

    users tenancies api added

--- controllers/tenancyAgreementController.js
+++ controllers/tenancyAgreementController.js
@@ -26,6 +26,19 @@ router.get('/:id', async (req, res, next) => {
         }
     }
 });
+router.get('/userTenancies/:userID', async (req, res, next) => {
+    const userId = parseInt(req.params.userID);
+    if (isNaN(userId) || userId < 1)
+        res.status(500).send('Neprimeren ID');
+    else {
+        try {
+            const data = await new dbHelper.tenancyAgreement().query('where', 'user_id', '=', userId.toString()).fetchAll();
+            res.json(data.toJSON());
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    }
+});
 
 router.post('/', (req, res, next) => {
     Joi.validate(req.body, tenancyAgreementModel, async function (err, value) {
