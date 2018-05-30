fdfd98ca681e0e14adbb25279f51bc2cbdfcab82
Jaka Tertinek <tertinek.jaka@gmail.com>
2018-05-30 18:50:31 +0200

    :(

--- controllers/utilityBillController.js
+++ controllers/utilityBillController.js
@@ -99,18 +99,18 @@ router.put('/:id', (req, res, next) => {
     });
 });
 
-router.put('/paid/:id', (req, res, next) => {
+/*router.put('/paid/:id', (req, res, next) => {
     const id = parseInt(req.params.id);
 
     if (isNaN(id) || id < 1)
         res.status(500).send('Neprimeren ID');
     try {
-        await new dbHelper.utilityBill({id: id.toString()}).save({paid: 1}, { method: 'update' });
+        await new dbHelper.utilityBill({id: id.toString()}).save({paid: 1});
         res.end();
     } catch (error) {
         res.status(500).json(error);
     }
-});
+});*/
 
 router.delete('/:id', async (req, res, next) => {
     const id = parseInt(req.params.id);
